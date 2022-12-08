8aa295ee47a27356b27ae7ccf89b628347858b03
Irina Rafalskaya <irena.narod@mail.ru>
2022-12-08 12:48:12 +0300

    fibonacci

--- Семинар3/Task5.py
+++ Семинар3/Task5.py
@@ -1,19 +1,17 @@
-#Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 N = int(input('Введите число: '))
-
 def fibonacci(N):
     fibo_num = []
     a, b = 1, 1
-    for i in range(N-1):
+    for i in range(N - 1):
         fibo_num.append(a)
         a, b = b, a + b
     a, b = 0, 1
-    for i in range (N):
+    for i in range(N):
         fibo_num.insert(0, a)
         a, b = b, a - b
     return fibo_num
-
 fibo_num = fibonacci(N)
 print(fibonacci(N))
 print(fibo_num.index(0))
